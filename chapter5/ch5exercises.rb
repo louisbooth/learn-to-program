ceb41fa250ca7f68b895f964215285c9a1341399
Louis Booth <louis.booth@hotmail.com>
2018-02-11 19:35:35 +0000

    Ch 5 Exercises

---
+++ chapter5/ch5exercises.rb
@@ -0,0 +1,13 @@
+puts 'What\'s your first name?'
+first_name = gets.chomp
+puts 'What\'s your middle name?'
+mid_name = gets.chomp
+puts 'What\'s your last name?'
+last_name = gets.chomp
+
+full_name = first_name + ' ' + mid_name + ' ' + last_name
+
+puts 'Hello, ' + full_name + '!'
+
+
+
