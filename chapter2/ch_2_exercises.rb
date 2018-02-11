4857feeb1c7680c9855e501618b6e2967a2f03e6
Louis Booth <louis.booth@hotmail.com>
2018-02-11 05:09:52 +0000

    redo

---
+++ chapter2/ch_2_exercises.rb
@@ -0,0 +1,11 @@
+# How many hours are in a year?
+hours_in_a_year = 24*365
+puts hours_in_a_year
+
+# How many minutes are in a decade?
+minutes_in_a_decade = 10 * (hours_in_a_year * 60)
+puts minutes_in_a_decade
+
+# How many seconds old are you?
+seconds_old_am_i = 33 * (60 * (hours_in_a_year * 60))
+puts seconds_old_am_i
