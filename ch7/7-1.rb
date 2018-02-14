482b4dd1dde6151841992836f894def7b346f0b6
Louis Booth <louis.booth@hotmail.com>
2018-02-14 22:16:36 +0000

    Comparison methods

---
+++ ch7/7-1.rb
@@ -0,0 +1,14 @@
+# Comparison methods
+
+puts 1 > 2
+puts 1 < 2
+puts 5 >= 5
+puts 5 <= 4
+
+puts 'cat' < 'dog'
+
+puts 'bug lady' < 'Xander'
+puts 'bug lady'.downcase < 'Xander'.downcase
+
+puts 2 < 10
+puts '2' < '10'
