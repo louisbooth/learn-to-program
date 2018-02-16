ba7a9bb00f68210aa50a963b4150d65ed2105842
Louis Booth <louis.booth@hotmail.com>
2018-02-16 06:32:44 +0000

    Leap Years

---
+++ ch7/7-5c.rb
@@ -0,0 +1,38 @@
+# Leap Years
+
+puts 'Pick a starting year (like 1973 or something):'
+starting = gets.chomp.to_i
+
+puts 'Now pick an ending year:'
+ending = gets.chomp.to_i
+
+puts 'Check it out... these years are leap years:'
+
+year = starting
+
+while year <= ending
+    if year%4 == 0
+        if year%100 != 0 || year%400 == 0
+            puts year
+        end
+    end
+    
+    year = year + 1
+end 
+
+
+# Alternate version
+
+puts 'Pick a starting year (like 1973 or something):'
+starting = gets.chomp.to_i
+
+puts 'Now pick an ending year:'
+ending = gets.chomp.to_i
+
+puts 'Check it out... these years are leap years:'
+
+(starting..ending).each do |year|
+    next if year%4 != 0
+    next if year%100 == 0 && year %400 != 0
+    puts year
+end 
