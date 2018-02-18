8dec773c3113bf101fe1114519b0c8dbb29cea15
Louis Booth <louis.booth@hotmail.com>
2018-02-18 00:38:31 +0000

    Experiment: Duby

---
+++ ch9/9-3.rb
@@ -0,0 +1,20 @@
+# Experiment: Duby
+
+def square x
+    puts (x * x)
+end
+
+x = 5
+square x
+
+my_number = 5
+square my_number
+
+x = 10
+my_number = x / 2
+square my_num
+
+
+def square asdf
+    asdf * asdf
+end
